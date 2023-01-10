e35264bd7e4fc0631f9532a080caa019f8a24c41
alexrod94 <alexrod94@gmail.com>
2023-01-10 15:40:44 +0100

    Add podcasts

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function Header() {
+  return (
+    <>
+      <Link href="/">
+        <h2 className="text-blue-500 text-xl font-semibold p-3">Podcaster</h2>
+      </Link>
+      <hr className="mb-3" />
+    </>
+  );
+}
+
+export default Header;
