8977070f5c842e21d057249491a43e97b9b44747
alexrod94 <alexrod94@gmail.com>
2023-01-11 11:58:03 +0100

    Reset webpack

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-module.exports = (nextConfig = {}, argv) => {
-  const isEnvDevelopment =
-    argv.mode === "development" || process.env.NODE_ENV === "development";
-  return Object.assign({}, nextConfig, {
-    webpack: (config, options) => {
-      config.mode = isEnvDevelopment ? "development" : "production";
-      config.optimization.minimize = !isEnvDevelopment;
-      return config;
-    },
-  });
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
